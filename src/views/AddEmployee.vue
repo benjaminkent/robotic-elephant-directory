63c09d2c08f1741c0658c918991344aea750a077
benjaminkent <benjamin.k.jehl@gmail.com>
2019-06-09 17:11:31 -0400

    style for adding is done

--- src/views/AddEmployee.vue
+++ src/views/AddEmployee.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   .add-container
-    form(@submit.prevent="addEmployee")
-      .form-group
-        label(for="firstName") First Name
-        input(id="firstName" type="text" placeholder="E.g. Bob" v-model="firstName")
-      .form-group
-        label(for="lastName") Last Name
-        input(id="lastName" type="text" placeholder="E.g. Smith" v-model="lastName")
-      button(type="submit") Create Employee
+    .header
+      p.header-text(v-if="!createdOk") Create a new employee
+      p.notifier(v-if="createdOk") Employee Successfully Created
+    .form-groups-container
+      form(@submit.prevent="addEmployee")
+        .form-group
+          label(for="firstName") First Name
+          input(id="firstName" type="text" placeholder="E.g. Bob" v-model="firstName")
+        .form-group
+          label(for="lastName") Last Name
+          input(id="lastName" type="text" placeholder="E.g. Smith" v-model="lastName")
+        button(type="submit") Create Employee
 </template>
 
 <script>
@@ -42,13 +46,64 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../design/variables.scss';
+
 .add-container {
-  color: #0a0a0a;
-  padding: 20px;
+  color: $black;
+  padding: 0 20px 20px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-groups-container {
+  width: 70%;
+  display: flex;
+  justify-content: center;
+}
+
+.notifier {
+  color: $green;
+}
+
+form {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  display: flex;
+  justify-content: space-between;
+  margin: 2px 0;
 }
 
 label {
   margin-right: 10px;
+}
+
+input {
+  width: 50%;
+  border: 1px solid #d8d8d8;
+  border-radius: 5px;
+  text-indent: 7px;
+}
+
+button {
+  font-size: 14px;
+  padding: 7px 0;
+  background-color: $neon-green;
+  border: 1px solid $neon-green;
+  border-radius: 5px;
+  color: #222;
+  cursor: pointer;
+  margin-top: 20px;
+}
+
+button:hover {
+  background-color: #222;
+  border: 1px solid #222;
+  color: $neon-green;
 }
 </style>
 
--- src/views/AllEmployees.vue
+++ src/views/AllEmployees.vue
@@ -4,7 +4,7 @@
       li(v-for="employee in employees")
         .header
           h3 {{ employee.firstName }} {{ employee.lastName }}
-          i.fal.fa-times-circle
+          i.fal.fa-times-circle(@click="deleteEmployee(employee)")
 </template>
 
 <script>
@@ -19,15 +19,48 @@ export default {
     this.axios.wrapper
       .get('/Employees')
       .then(resp => this.employees = resp.data)
+  },
+  methods: {
+    deleteEmployee (employee) {
+      this.axios.wrapper
+        .delete(`/Employees/${employee.id}`)
+        .then(response => console.log(response))
+      this.employees.splice(this.employees.indexOf(employee), 1)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+li {
+  width: 300px;
+  margin: 20px;
+}
+
 .header {
+  display: flex;
+  justify-content: space-between;
+
   h3 {
     margin: 0;
   }
+}
+
+.fa-times-circle {
+  color: #ff3f3f;
+  cursor: pointer;
+}
+
+.fa-times-circle:hover {
+  color: #bc2f2f;
 }
 </style>
 
