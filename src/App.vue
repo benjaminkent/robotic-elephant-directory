15cf678ffa31ada6d491e5b5506cc515e5b3f2f8
benjaminkent <benjamin.k.jehl@gmail.com>
2019-06-09 12:41:55 -0400

    added nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template lang="pug">
   .app-container
     Hero
+    nav
+      ul
+        li
+          router-link(to="#") All Employees
+        li
+          router-link(to="#") Add Employee
     router-view
 </template>
 
@@ -17,11 +23,37 @@ export default {
 
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Righteous&display=swap');
+@import './design/variables.scss';
+
 body {
   margin: 0;
 }
 
 .app-container {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  background-color: #222;
+  padding: 20px 0;
+}
+
+ul {
+  width: 70%;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+
+  li {
+
+    a {
+      color: $neon-green;
+    }
+  }
 }
 </style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
   .hero-container
     .hero-grid
+    .header-grid
+      header
+        h1 Robotic Elephant
+        p Employee Directory
 </template>
 
 <script>
@@ -23,6 +27,29 @@ export default {
     background-image: url("../assets/robot-elephant.jpg");
     background-position: top;
     background-size: cover;
+  }
+
+  .header-grid {
+    grid-area: 1 / 1 / 2 / 4;
+    justify-self: center;
+    align-self: end;
+
+    header {
+      font-family: 'Righteous', cursive;
+      color: #47ffb8;
+      text-shadow: 1px 1px 1px #0a0a0a;
+
+      h1 {
+        font-size: 42px;
+        margin: 0;
+      }
+
+      p {
+        margin: 0;
+        text-align: center;
+        font-size: 26px;
+      }
+    }
   }
 }
 </style>
